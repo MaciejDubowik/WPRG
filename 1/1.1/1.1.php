eaff2799774fd6d21c75ea34167114954bfa4a4d
MaciejDubowik <77201172+MaciejDubowik@users.noreply.github.com>
2021-03-17 15:42:53 +0100

    Add files via upload

---
+++ 1/1.1/1.1.php
@@ -0,0 +1,8 @@
+<?php
+function rzutKostka(){
+$rzut = rand(1,6);
+return $rzut;
+}
+echo rzutKostka();
+
+?>
---
+++ 1/1.2/1.2.php
@@ -0,0 +1,8 @@
+<?php
+function srednicaKola($promien){
+$srednica = 2 * $promien;
+return $srednica;
+}
+echo srednicaKola(10);
+
+?>
