c60b1cb5743fe23212de05935f81be75d0c192a9
MaciejDubowik <77201172+MaciejDubowik@users.noreply.github.com>
2021-03-31 00:31:06 +0200

    Add files via upload

---
+++ 2/2.2/2.2.php
@@ -0,0 +1,14 @@
+<?php
+
+$obywatel = array(
+    'Szwecja' => 'Szwed',
+    'Polska' => 'Polak',
+    'Niemcy' => 'Niemiec',
+    'Norwegia' => 'Norweg',
+);
+
+$panstwo = 'Polska';
+
+echo $obywatel[$panstwo];
+
+?>
